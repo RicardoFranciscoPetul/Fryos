a745b6b9c0a899c44b7325851f1f24c0f76f3926
Ricardo Francisco <ricardofco.35@gmail.com>
2020-09-10 13:06:27 -0500

    Title component

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const Title = ({ text, style, ...props }) => {
+  let textStyles = styles.primary;
+  if (props.type === 'secondary') textStyles = styles.secondary;
+
+  return (
+    <Text style={[textStyles, style]} {...props} type={props.type}>
+      {text}
+    </Text>
+  );
+};
+
+Title.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['primary', 'secondary']),
+  style: PropTypes.any,
+};
+
+Title.defaultProps = {
+  type: 'primary',
+};
+
+export default Title;
---
+++ src/components/Title/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  primary: {
+    color: '#000',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  secondary: {
+    color: '#000',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
