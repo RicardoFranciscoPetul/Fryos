bdac777259205e3d7d3af962f46b8e2bc64cb2d8
Ricardo Francisco <ricardofco.35@gmail.com>
2020-09-10 13:07:42 -0500

    Screens :sparkles:

--- src/screens/Bookings/Details/index.jsx
+++ src/screens/Bookings/Details/index.jsx
@@ -1,12 +1,102 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useContext } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+} from 'react-native';
+import { Container, SubCategory, Title, PopUp } from '../../../components';
+import { Colors } from '../../../constants';
+import bookingContext from '../../../context/Booking/context';
 
-const Detalles = () => {
+const BookingDetails = ({ route, navigation }) => {
+  const BookingContext = useContext(bookingContext);
+  const { makeBooking } = BookingContext;
+
+  const { item } = route.params;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentItem, setCurrentItem] = useState({});
+
+  const onPress = item => {
+    if (!item.available) return;
+    setModalVisible(true);
+    setCurrentItem(item);
+    // setModalVisible(false)
+  };
+
+  const reserve = () => {
+    makeBooking(item, currentItem);
+    navigation.goBack();
+  };
   return (
-    <View>
-      <Text>Detalles</Text>
-    </View>
+    <ScrollView>
+      <Container>
+        <PopUp
+          isVisible={modalVisible}
+          schedule={currentItem}
+          item={item}
+          onPress={reserve}
+          onClose={() => setModalVisible(false)}
+        />
+        <Title text='Seleccionar categoria' />
+        <FlatList
+          data={item.subcategories}
+          renderItem={({ item }) => <SubCategory name={item.name} />}
+          keyExtractor={(item, index) => index.toString()}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ marginTop: 10 }}
+        />
+        <Title
+          type='secondary'
+          style={styles.secondaryTitle}
+          text='Observaciones'
+        />
+        <Text>{item.observations}</Text>
+        <Title
+          type='secondary'
+          style={styles.secondaryTitle}
+          text='Valor de la reserva'
+        />
+        <Text>{item.price}</Text>
+        <Title
+          type='secondary'
+          style={styles.secondaryTitle}
+          text='Horario disponible'
+        />
+        {item.schedule.schedules.map((item, index) => (
+          <TouchableOpacity key={index} onPress={() => onPress(item)}>
+            <Title
+              style={[styles.schedule, !item.available && styles.noAvailable]}
+              text={`${item.start} - ${item.end}`}
+            />
+          </TouchableOpacity>
+        ))}
+      </Container>
+    </ScrollView>
   );
 };
 
-export default Detalles;
+const styles = StyleSheet.create({
+  secondaryTitle: {
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  schedule: {
+    borderColor: Colors.blue,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  noAvailable: {
+    backgroundColor: Colors.gray,
+    opacity: 0.5,
+  },
+});
+
+export default BookingDetails;
--- src/screens/Bookings/index.jsx
+++ src/screens/Bookings/index.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { View, FlatList, Text } from 'react-native';
+import { Category, Container, Booking, Title } from '../../components';
+
+import bookingContext from '../../context/Booking/context';
 
 const Bookings = ({ navigation }) => {
+  const BookingContext = useContext(bookingContext);
+  const { categories, bookings } = BookingContext;
+
+  const goToBooking = item => navigation.navigate('BookingsDetails', { item });
+
   return (
-    <View>
-      <Text>Bookings</Text>
-      <Button
-        title='Ir'
-        onPress={() => navigation.navigate('BookingsDetails')}
+    <Container>
+      <FlatList
+        data={categories}
+        renderItem={({ item }) => (
+          <Category item={item} onPress={() => goToBooking(item)} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        ListHeaderComponent={<Title text='Categorias' />}
+        ListFooterComponent={
+          <FlatList
+            data={bookings}
+            renderItem={({ item }) => <Booking item={item} />}
+            keyExtractor={(item, index) => index.toString()}
+            ListHeaderComponent={<Title text='Mis reservas' />}
+            ListFooterComponent={<View style={{ height: 150 }} />}
+            ListEmptyComponent={
+              <Text type='secondary' text=''>
+                Selecciona una categoria y has una reserva.
+              </Text>
+            }
+          />
+        }
       />
-    </View>
+    </Container>
   );
 };
 
